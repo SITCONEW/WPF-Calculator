8c61b2a7345cc07a741bf97d4aa21f6c966205be
USRSIT0117 <USRSIT0117@ESTSIT0117>
2022-10-13 09:58:39 -0400

    Agregar archivos de proyecto.

---
+++ WPF-Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// La información general sobre un ensamblaje está controlada por
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociado con un ensamblado.
+
+[assembly: AssemblyTitle("WPF-Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WPF-Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Establecer ComVisible en falso hace que los tipos en este ensamblaje sean invisibles
+// para componentes COM. Si necesita acceder a un tipo en este ensamblaje desde
+// COM, establezca el atributo ComVisible en verdadero en este tipo.
+[assembly: ComVisible(false)]
+
+// Para comenzar a compilar aplicaciones localizables, configure
+//<UICulture>CultureYouAreCodingWith</UICulture> en su archivo .csproj
+//dentro de un <Grupo de Propiedades>. Por ejemplo, si está utilizando el idioma inglés
+//en sus archivos fuente, establezca <UICulture> en en-US. luego descomentar
+//el atributo NeutralResourceLanguage a continuación. Actualice el "en-US" en
+// línea a continuación para que coincida con la configuración de UICulture en el archivo del proyecto.
+
+//[ensamblado: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //dónde se encuentran los diccionarios de recursos específicos del tema
+                                     //(se usa si no se encuentra algún recurso en la página,
+                                     // o diccionarios de recursos de la aplicación)
+    ResourceDictionaryLocation.SourceAssembly //dónde se encuentra el diccionario de recursos genéricos
+                                              //(se usa si no se encuentra algún recurso en la página,
+                                              // aplicación, o cualquier otro diccionario de recursos específico del tema)
+)]
+
+
+// La información de versión de un ensamblaje consta de los siguientes cuatro valores:
+//
+// Versión 1.0
+// Version 1.0
+// v 1.0
+// Revisión: Isaac Rodriguez
+//
+// Puede especificar todos los valores o por defecto a Números de compilación y revisión
+// usando el "*" como se muestra a continuación:
+// [ensamblado: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
